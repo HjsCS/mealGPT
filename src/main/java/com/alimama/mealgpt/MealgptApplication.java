79bd52ad7730f5cc4232af9e10d74cdc17e62818
HjsCS <127371829+HjsCS@users.noreply.github.com>
2023-09-04 21:06:55 +1000

    update port for fl0

--- src/main/java/com/alimama/mealgpt/MealgptApplication.java
+++ src/main/java/com/alimama/mealgpt/MealgptApplication.java
@@ -3,16 +3,12 @@ package com.alimama.mealgpt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
-
 @SpringBootApplication
 public class MealgptApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(MealgptApplication.class);
-		String port = System.getenv("PORT");
-		app.setDefaultProperties(Collections.singletonMap("server.port", port == null ? "8080" : port));
-		app.run(args);
+		SpringApplication.run(MealgptApplication.class, args);
+
 	}
 
 }
