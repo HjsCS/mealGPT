9a978ebbe6cd2b6ee6b3d027c76dca4af7b8e667
HjsCS <127371829+HjsCS@users.noreply.github.com>
2023-10-28 16:05:20 +1100

    feat: add unit tests

--- src/main/java/com/alimama/mealgpt/pojo/LoginRequest.java
+++ src/main/java/com/alimama/mealgpt/pojo/LoginRequest.java
@@ -1,14 +1,12 @@
 package com.alimama.mealgpt.pojo;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @ToString
 public class LoginRequest {
     private String userName;
--- src/main/java/com/alimama/mealgpt/pojo/RegisterRequest.java
+++ src/main/java/com/alimama/mealgpt/pojo/RegisterRequest.java
@@ -3,14 +3,12 @@ package com.alimama.mealgpt.pojo;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @ToString
 public class RegisterRequest {
 
